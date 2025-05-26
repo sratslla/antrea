07f615ef71520d5ee663d5aeefa1c866769f548b
sratslla <shubhgaur.cse24@jecrc.ac.in>
2025-05-27 03:43:07 +0530

    Add PacketCapture test resources and agent logs for proposal #6861
    This commit adds test instrumentation and required artifacts for the
    PacketCapture proposal task:
    
    - Added structured log entries to antrea-agent and antrea-controller
    - Deployed Antrea with PacketCapture enabled to Kind cluster
    - Captured TCP SYN traffic to port 53 using PacketCapture CR
    - Included the resulting PCAP file and tcpdump output
    - Logged Antrea Pod features and OVS version
    - Added all YAML manifests and outputs as required by task #6861
    
    Signed-off-by: sratslla <shubhgaur.cse24@jecrc.ac.in>

--- cmd/antrea-agent/agent.go
+++ cmd/antrea-agent/agent.go
@@ -84,6 +84,7 @@ import (
 	"antrea.io/antrea/pkg/ovs/ovsctl"
 	"antrea.io/antrea/pkg/signals"
 	"antrea.io/antrea/pkg/util/channel"
+	"antrea.io/antrea/pkg/util/env"
 	"antrea.io/antrea/pkg/util/k8s"
 	"antrea.io/antrea/pkg/util/lazy"
 	"antrea.io/antrea/pkg/util/podstore"
@@ -108,6 +109,7 @@ var ipv4Localhost = net.ParseIP("127.0.0.1")
 // run starts Antrea agent with the given options and waits for termination signal.
 func run(o *Options) error {
 	klog.InfoS("Starting Antrea Agent", "version", version.GetFullVersion())
+	klog.InfoS("Added by Shubh Gaur", "username", "sratslla", "podName", env.GetPodName())
 
 	// Create K8s Clientset, CRD Clientset, Multicluster CRD Clientset and SharedInformerFactory for the given config.
 	k8sClient, _, crdClient, _, mcClient, _, err := k8s.CreateClients(o.config.ClientConnection, o.config.KubeAPIServerOverride)
